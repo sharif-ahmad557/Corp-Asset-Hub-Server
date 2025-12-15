1f62131995844f645b929e2671f367bd3807f56b
Shariful Islam <sharifahmadwd@gmail.com>
2025-12-15 23:54:13 +0600

    all is ok in server site

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
+      "http://localhost:5175",
       "http://localhost:5173",
-      "https://corp-asset-hub-client-gcfh.vercel.app",
     ],
     credentials: true,
   })
